93ecf66e53d5e2e4df2c0c4553d626e0e1a72e8c
RaturiAR7 <anshulraturi007@gmail.com>
2026-02-18 17:20:33 +0530

    backend setup

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is running 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
