a3b234cb094f716fa46ee7d7fe0f62baae51c01b
Anshul <anshulraturi007@gmail.com>
2026-03-06 23:07:25 +0530

    develop add project member api

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -60,3 +60,75 @@ export const getMyProjects = async (req: Request, res: Response) => {
     });
   }
 };
+
+interface Params {
+  projectId: string;
+}
+
+export const addProjectMember = async (req: Request<Params>, res: Response) => {
+  try {
+    const { projectId } = req.params;
+    const { userId } = req.body;
+
+    if (!userId && !projectId) {
+      return res.status(400).json({
+        message: "userId is required",
+      });
+    }
+
+    // Check project exists
+    const project = await prisma.project.findUnique({
+      where: { id: projectId },
+    });
+
+    if (!project) {
+      return res.status(404).json({
+        message: "Project not found",
+      });
+    }
+
+    // Check user exists
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    // Check already member
+    const existingMember = await prisma.projectMember.findUnique({
+      where: {
+        projectId_userId: {
+          projectId,
+          userId,
+        },
+      },
+    });
+
+    if (existingMember) {
+      return res.status(400).json({
+        message: "User is already a project member",
+      });
+    }
+
+    const member = await prisma.projectMember.create({
+      data: {
+        projectId,
+        userId,
+      },
+    });
+
+    return res.status(201).json({
+      message: "Member added successfully",
+      member,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
