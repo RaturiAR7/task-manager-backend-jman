60894d15359a4f960aa39a3d623af01b487adea3
Anshul <anshulraturi007@gmail.com>
2026-03-09 23:13:31 +0530

    develop get me api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -102,3 +102,28 @@ export const getAllUsers = async (
     });
   }
 };
+
+export const getMe = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const userId = (req as any).user.id;
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
